7002c3f07287fdaa529cf66ca87f101689309984
KaHei Ng <kaheicanaan@gmail.com>
2019-02-10 20:36:34 +0800

    add (1) gitconfig (2) base16-shell

--- install.sh
+++ install.sh
@@ -6,9 +6,17 @@ DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 git submodule update --init --recursive
 git submodule update --remote --recursive
 
+# Base16
+mkdir -p "${HOME}/.config"
+rm -rf "${HOME}/.config/base16-shell"
+ln -sF "${DIR}/base16-shell" "${HOME}/.config/base16-shell"
+
 # Zsh
 ln -sF "${DIR}/zshrc" "${HOME}/.zshrc"
 
 # Prezto
 ln -sF "${DIR}/prezto" "${HOME}/.zprezto"
 ln -sF "${DIR}/zpreztorc" "${HOME}/.zpreztorc"
+
+# Git
+ln -sF "${DIR}/gitconfig" "${HOME}/.gitconfig"
--- zshrc
+++ zshrc
@@ -1,1 +1,8 @@
 source "${ZDOTDIR:-$HOME}/.zprezto/init.zsh"
+
+# Base16
+BASE16_SHELL="${HOME}/.config/base16-shell"
+[ -n "${PS1}" ] && \
+  [ -s "${BASE16_SHELL}/profile_helper.sh" ] && \
+  eval "$("${BASE16_SHELL}/profile_helper.sh")"
+base16_default-dark
